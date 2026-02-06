e7ce477616a43f6097fbd359b5ef1f293bb46cf3
Elliot Harris <elliotharris44@hotmail.co.uk>
2026-02-06 12:39:32 +0100

    Before making start at psi4 max

--- functions.py
+++ functions.py
@@ -1,5 +1,18 @@
 import numpy as np
 
+def ringdown_comb(omegas, taus):
+    def model(t, *params):
+        start = len(t)//2
+        sig = np.zeros_like(t)
+        N = len(params)//2
+        for n in range(N):
+            A = params[2*n]
+            phi = params[2*n+1]
+            sig[:start] += A * np.exp(-t[:start]/taus[n]) * np.cos(omegas[n]*t[:start] + phi)
+            sig[start:] += -A * np.exp(-t[:start]/taus[n]) * np.sin(omegas[n]*t[:start] + phi)
+        return sig
+    return model
+
 def ringdown_real(omegas, taus):
     def model(t, *params):
         sig = np.zeros_like(t)
@@ -18,6 +31,6 @@ def ringdown_imag(omegas, taus):
         for n in range(N):
             A = params[2*n]
             phi = params[2*n+1]
-            sig += A * np.exp(-t/taus[n]) * np.sin(omegas[n]*t + phi)
+            sig += -A * np.exp(-t/taus[n]) * np.sin(omegas[n]*t + phi)
         return sig
     return model
